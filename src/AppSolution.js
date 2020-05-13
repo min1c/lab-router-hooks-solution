b7706901acd877aad896da05de19df321ec5f9f6
Dominic F. Villar <dominic.f.villar@avanade.com>
2020-05-13 13:28:53 -0400

    Add callback hook and update instructions

--- src/AppSolution.js
+++ src/AppSolution.js
@@ -5,34 +5,33 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
+  NavLink,
 } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
-import { Films } from './FilmHookSolution';
+import * as C from './FilmConstants'
+import { Films } from './FilmsHookSolution';
 
 export default function AppSolution() {
+
+  // 1. Modify the behaviour of the Navbar.Brand and Nav.Link elements to behave as a react-router <NavLink>.
+  //    Using <NavLink> will not trigger a full page refresh when navigating within a router. 
+  //    Hint: Use 'as' attribute
+  // 2. Replace the current <Home> return with  two <Route> components within a <Switch> component
+  //    a. The first route will render the <Films> component if the path is "/films"
+  //    b. The second route will render the <Home> component if the path is "/"
+
   return (
     <div>
       <Router>
         <Navbar bg="dark" variant="dark">
-          {
-            // 1. Modify the behaviour of the Navbar.Brand and Nav.Link elements to behave as a react-router
-            //    <Link>. Using <Link> will not trigger a full page refresh when navigating within a router. 
-            //    Hint: Use 'as' attribute
-          }
-          <Navbar.Brand as={Link} to="/home">Lab 3 (Router)</Navbar.Brand>
+          <Navbar.Brand as={NavLink} to="/home">Lab 3 (Router)</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-            <Nav.Link as={Link} to="/films">Films</Nav.Link>
+            <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/films">Films</Nav.Link>
           </Nav>
         </Navbar>
-        {
-          // 2. Replace the current <Home> return with  two <Route> components within a <Switch> component
-          // a. The first route will render the <Films> component if the path is "/films/:id?" or "/films"
-          // b. The second route will render the <Home> component if the path is "/"
-        }
         <Switch>
-          <Route path={["/films/:id?", "/films"]} component={Films} />
+          <Route path="/films" component={Films} />
           <Route path="/">
             <Home />
           </Route>
@@ -46,6 +45,17 @@ function Home() {
   return (
     <Container>
       <h3 className="display-3">Home</h3>
-      <p>This lab aims to apply the basics of react-router.</p>
+      <p>This lab aims to apply the basics of:</p>
+      <ul>
+        <li>event-binding</li>
+        <li>state</li>
+        <li>lifecycle methods</li>
+        <li>routing</li>
+      </ul>
+      <ol>
+        {C.INCOMPLETE_1_HTML}<br/>
+        {C.INCOMPLETE_2_HTML}<br/>
+        {C.INCOMPLETE_3_HTML}
+      </ol>
     </Container>);
 }
